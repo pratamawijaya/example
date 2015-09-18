8478e25b5ae777d7733694f52c5e0ef430ed89b4
pratamawijaya <set.mnemonix@gmail.com>
2015-09-18 11:47:40 +0700

    update method, remove old project

--- ExampleInstagram/app/src/main/java/com/pratamawijaya/exampleinstagram/BaseApplication.java
+++
@@ -1,9 +0,0 @@
-package com.pratamawijaya.exampleinstagram;
-
-import android.app.Application;
-
-/**
- * Created by pratama on 12/12/14.
- */
-public class BaseApplication extends Application {
-}
--- ExampleInstagram/app/src/main/java/com/pratamawijaya/exampleinstagram/authentication/InstagramApp.java
+++
@@ -1,8 +0,0 @@
-package com.pratamawijaya.exampleinstagram.authentication;
-
-/**
- * Created by pratama on 12/12/14.
- */
-public class InstagramApp {
-    public static final String CALLBACKURL = "";
-}
